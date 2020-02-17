8dfe090c58a1eb21791b71f3bc201936e01564c8
nicoman35 <christian_nicolau@yahoo.de>
2020-02-17 16:48:48 +0100

    Update config.cpp

--- Addons/config.cpp
+++ Addons/config.cpp
@@ -12,13 +12,15 @@ class CfgFunctions {
 		class base {
 			class InitRearmMK6 {
 				preInit = 1;
-				file="\NIC_rearm_MK6\scripts\init.sqf";
+				file	= "\NIC_rearm_MK6\scripts\init.sqf";
 			};
 		};
 	};
 };
 class Extended_PreInit_EventHandlers {
-	Test1 = call compile preprocessFileLineNumbers "\NIC_rearm_MK6\scripts\XEH_preInit.sqf"; // CBA_a3 integration
+	class NIC_MK6_mortar {
+		init = "call compile preprocessFileLineNumbers '\NIC_rearm_MK6\scripts\XEH_preInit.sqf'"; // CBA_a3 integration
+	};
 };
 class Extended_GetIn_Eventhandlers {
     class Mortar_01_base_F {
